078743afc92762f8131766ef6a2fe7c42a2ca8cc
Alexandru Scvortov <scvalex@gmail.com>
2011-07-04 00:25:30 +0100

    refactoring

--- Network/AMQP/Framing.hs
+++ Network/AMQP/Framing.hs
@@ -9,32 +9,6 @@ import Network.AMQP.FramingTypes
 import Network.AMQP.Types
 
 import Data.Binary ( Binary(..) )
-import Data.Binary.Get ( Get, getWord8 )
-import Data.Binary.Put ( Put, putWord8 )
-import Data.Bits
-import Data.Word ( Word8 )
-
--- * Bits need special handling because AMQP requires contiguous bits
--- to be packed into a Word8
-
--- | Packs up to 8 bits into a Word8
-putBits :: [Bit] -> Put
-putBits = putWord8 . putBits' 0
-    where
-      putBits' _ [] = 0
-      putBits' offset (x:xs) =
-          (shiftL (toInt x) offset) .|. (putBits' (offset+1) xs)
-              where
-                toInt True = 1
-                toInt False = 0
-
-getBits :: Integer -> Get [Bit]
-getBits num = getWord8 >>= \x -> return $ getBits' num 0 x
-    where
-      getBits' :: Integer -> Integer -> Word8 -> [Bit]
-      getBits' 0 _ _ = []
-      getBits' n offset x =
-          ((x .&. (2 ^ offset)) /= 0) : (getBits' (n-1) (offset+1) x)
 
 $(genContentHeaderProperties domainMap classes)
 
--- Network/AMQP/Helpers.hs
+++ Network/AMQP/Helpers.hs
@@ -1,45 +1,51 @@
-module Network.AMQP.Helpers where
+module Network.AMQP.Helpers
+    ( toStrict, toLazy
+    , Lock, newLock, openLock, closeLock, waitLock, killLock
+    ) where
 
+import Control.Applicative ( Applicative(..), (<$>) )
 import Control.Concurrent.MVar
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy.Char8 as BL
 
-
+-- | Convert a lazy ByteString to a strict one.
 toStrict :: BL.ByteString -> BS.ByteString
 toStrict x = BS.concat $ BL.toChunks x
 
+-- | Convert a strict ByteString to a lazy one.
 toLazy :: BS.ByteString -> BL.ByteString
 toLazy x = BL.fromChunks [x]
 
-
-    
--- if the lock is open, calls to waitLock will immediately return. if it is closed, calls to waitLock will block. if the lock is killed, it will always be open and can't be closed anymore
+-- If the lock is open, calls to waitLock will immediately return.  If
+-- it is closed, calls to waitLock will block.  If the lock is killed,
+-- it will always be open and can't be closed anymore.
 data Lock = Lock (MVar Bool) (MVar ())
 
+-- | Create an (alive, open) lock.
 newLock :: IO Lock
-newLock = do
-    a <- newMVar False
-    b <- newMVar ()
-    return $ Lock a b 
-     
-openLock :: Lock -> IO ()
-openLock (Lock _ b) = do
-    tryPutMVar b ()
-    return ()
+newLock = Lock <$> (newMVar False) <*> (newMVar ())
 
+-- | Open the given lock.  You may open a lock as many times as you
+-- please.
+openLock :: Lock -> IO ()
+openLock (Lock _ b) = tryPutMVar b () >> return ()
+
+-- | Close the given lock.  You may always close a lock, but closing a
+-- killed lock is a no-op.
 closeLock :: Lock -> IO ()
 closeLock (Lock a b) = do
-    withMVar a $ \killed ->
-        if killed 
-            then return ()
-            else tryTakeMVar b >> return ()
-    return ()
+  withMVar a $ \killed ->
+      if killed
+        then return ()
+        else tryTakeMVar b >> return ()
+  return ()
 
+-- | Wait until the given lock is open.
 waitLock :: Lock -> IO ()
 waitLock (Lock _ b) = readMVar b
 
+-- | Kill the given lock and open it.
 killLock :: Lock -> IO Bool
 killLock (Lock a b) = do
-    modifyMVar_ a $ \_ -> return True
-    tryPutMVar b ()
-    
+  modifyMVar_ a $ \_ -> return True
+  tryPutMVar b ()
