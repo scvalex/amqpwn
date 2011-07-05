81279e0dcbfad91fe7e9a51f8ce43532f5b943b9
Alexandru Scvortov <scvalex@gmail.com>
2011-07-06 00:10:02 +0100

    solve the 3 TH missing sig warnings
    The trick is to add the function signatures *after* the generated
    functions.  This is fine because GHC is smart enough to find them
    anywhere in the file, but they have to come after the function
    definitions because of the TH restriction.

--- Network/AMQP/Framing.hs
+++ Network/AMQP/Framing.hs
@@ -8,16 +8,19 @@ import Network.AMQP.FramingData
 import Network.AMQP.FramingTypes
 import Network.AMQP.Types
 
-import Data.Binary ( Binary(..) )
+import Data.Binary ( Binary(..), Get, Put )
 
 $(genContentHeaderProperties domainMap classes)
 
 $(genClassIDFuns classes)
+getClassIDOf :: (Num a) => ContentHeaderProperties -> a
 
 $(genMethodPayload domainMap classes)
 
 $(genGetContentHeaderProperties classes)
+getContentHeaderProperties :: (Num a) => a -> Get ContentHeaderProperties
 
 $(genPutContentHeaderProperties classes)
+putContentHeaderProperties :: ContentHeaderProperties -> Put
 
 $(genMethodPayloadBinaryInstance domainMap classes)
